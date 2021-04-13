9755d68cc4db550142aafc17c7cd62efa8c71d8c
Segun <olumajasegun@gmail.com>
2021-04-13 05:28:45 +0100

    Custom routes created

---
+++ VideoRental/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRental.Models;
+
+namespace VideoRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Castle&Castle" };
+            return View(movie);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
